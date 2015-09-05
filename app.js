05164280413620457624a86a93aa17197812ef63
Rahul Madduluri <rahulmadduluri@gmail.com>
2015-09-05 00:22:00 -0700

    send user on auth

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.post('/api/authenticate', function(req, res) {
 				res.json({
 					success: true,
 					message: 'Token successfully generated!',
-					uuid: user.uuid
+					user: user
 					token: token
 				});
 			}
